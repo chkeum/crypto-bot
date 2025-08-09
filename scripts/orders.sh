88d1c1cfc97964973fc453106844c37c60cbff91
chkeum <kch31406@gmail.com>
2025-08-10 03:21:52 +0900

    auto deploy

--- scripts/orders.sh
+++ scripts/orders.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sym="${1:-BTC/USDT:USDT}"
+sym="${1:-ETH/USDT:USDT}"
 curl -sS "http://127.0.0.1:8000/orders?symbol=${sym}" | jq . || curl -sS "http://127.0.0.1:8000/orders?symbol=${sym}"
